0e3345be453fd756dd90c523bc7dc606103db95d
Rack <ragontru25@gmail.com>
2020-11-24 19:50:33 +0100

    Test tema 1

---
+++ Tema1/src/ejemplos/Test.java
@@ -0,0 +1,11 @@
+package ejemplos;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		for (String str : args) {
+			System.out.println(str);
+		}
+	}
+}

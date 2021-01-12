61b38ea05057789c0a53743901e983aff5ab6773
Ragontru <ragontru25@gmail.com>
2021-01-12 14:41:44 +0100

    Cambios en cliente/servidor

--- Tema3/Practica/cliente/Cliente.java
+++ Tema3/Practica/cliente/Cliente.java
@@ -1,6 +1,10 @@
 package cliente;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 public class Cliente {
 
@@ -8,7 +12,20 @@ public class Cliente {
 		
 		try {
 			System.out.println("Creando socket CLIENTE");
-			Socket cliente
+			Socket clienteSocket = new Socket();
+			
+			System.out.println("Estableciendo la conexión");
+			InetSocketAddress addr = new InetSocketAddress("localhost",5555);
+			clienteSocket.connect(addr);
+			
+			InputStream is = clienteSocket.getInputStream();
+			OutputStream os = clienteSocket.getOutputStream();
+			
+			System.out.println("Enviando mensaje");
+			String mensaje = "Mensaje desde el cliente";
+			os.write(mensaje.getBytes());
+			
+			System.out.println("Mensaje enviado");
 			
 		} catch (IOException e) {
 			e.printStackTrace();
--- Tema3/Practica/servidor/Servidor.java
+++ Tema3/Practica/servidor/Servidor.java
@@ -1,8 +1,11 @@
 package servidor;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 
@@ -10,14 +13,30 @@ public class Servidor {
 		
 		try {
 			System.out.println("Creando socket SERVIDOR");
-			ServerSocket servidor = new ServerSocket();
+			ServerSocket server = new ServerSocket();
+			
+			System.out.println("Realizando el bind");
 			InetSocketAddress addr = new InetSocketAddress("localhost",5555);
-			servidor.bind(addr);
+			server.bind(addr);
 			
 			// Aceptar conexiones
+			/*
+			 * System.out.println("Aceptando conexiones");
+			Socket newSocket = server.accept();
+
+			System.out.println("Conexión recibida");
+			InputStream is = newSocket.getInputStream();
+			OutputStream os = newSocket.getOutputStream();
+
+			byte[] message = new byte[25];
+			is.read(message);
+			System.out.println("Mensaje recibido: " + new String(message));
+			 */
 			
-			servidor.close();
-			System.out.println("SERVIDOR cerrado");
+			System.out.println("Cerrando el socket SERVIDOR");
+			server.close();
+			
+			System.out.println("Terminado");
 						
 		} catch (IOException e) {
 			e.printStackTrace();
